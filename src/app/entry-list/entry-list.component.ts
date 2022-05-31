4bc239e0e22568cad2c96dd80f310608cc8b7a89
Jean Tessier <jean@jeantessier.com>
2022-05-30 17:52:20 -0700

    Define and use entry-list component

--- src/app/entry-list/entry-list.component.ts
+++ src/app/entry-list/entry-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-entry-list',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryListComponent implements OnInit {
 
+  @Input() entries!: any[];
   constructor() { }
 
   ngOnInit(): void {
